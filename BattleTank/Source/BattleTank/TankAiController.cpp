61f71da0ba87b2627b1c685724470784daf566b0
Marco Formoso <marco.a.formoso@gmail.com>
2017-10-12 13:58:04 +0200

    TankAIController working

---
+++ BattleTank/Source/BattleTank/TankAiController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAiController.h"
+#include "Engine/World.h"
+
+
+ATank*  ATankAiController::GetControllerTank() const {
+
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAiController::GetPlayerTank() const {
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
+void  ATankAiController::BeginPlay() {
+	Super::BeginPlay();
+
+	ATank* PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIPlayController not getting player tank"));
+	}
+	else UE_LOG(LogTemp, Warning, TEXT("AIPlayController getting player tank: %s"), *PlayerTank->GetName());
+
+
+}
+
---
+++ BattleTank/Source/BattleTank/TankAiController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Tank.h"
+#include "TankAiController.generated.h"
+
+UCLASS()
+class BATTLETANK_API ATankAiController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	void BeginPlay() override;
+private: 
+	ATank* GetControllerTank() const;
+	ATank* GetPlayerTank() const;
+	
+};
