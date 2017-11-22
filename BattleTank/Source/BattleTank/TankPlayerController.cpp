38f5ab4085afb8200f85b483773d599e0f5da0ac
Marco Formoso <marco.a.formoso@gmail.com>
2017-11-22 19:58:57 +0100

    Aiming improvement

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -22,7 +22,6 @@ void ATankPlayerController::AimTowardCrosshair() {
 	if (!ensure(AimingComponent)) { return; }
 	FVector HitLocation; // Out parameter
 	bool bGotHitLocation = GetSightRayHitLocation(HitLocation);
-	UE_LOG(LogTemp, Warning, TEXT("bGotHitLocation: %i"), bGotHitLocation)
 	if (bGotHitLocation) {
 		AimingComponent->AimAt(HitLocation);		
 	}
